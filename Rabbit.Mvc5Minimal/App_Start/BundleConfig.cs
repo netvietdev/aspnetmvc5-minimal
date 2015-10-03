795693461862248146e34d3916c361ad406de7d5
juanonsoftware <huanhvhd@gmail.com>
2015-10-03 13:44:30 +0700

    Version 20151003 - Use JQuery1 or JQuery2 automatically by browser - Upgrate to latest version of JQuery1 / JQuery2

--- Rabbit.Mvc5Minimal/App_Start/BundleConfig.cs
+++ Rabbit.Mvc5Minimal/App_Start/BundleConfig.cs
@@ -8,8 +8,11 @@ namespace Rabbit.Mvc5Minimal.App_Start
         {
             bundles.UseCdn = true;
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery", "//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.3.min.js")
-                .Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.4.min.js")
+                .Include("~/Scripts/jquery-2.*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery1", "//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.3.min.js")
+                .Include("~/Scripts/jquery-1.*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap", "//ajax.aspnetcdn.com/ajax/bootstrap/3.3.5/bootstrap.min.js")
                 .Include("~/Scripts/bootstrap.js"));
