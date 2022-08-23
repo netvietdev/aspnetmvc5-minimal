da6674020b74c0b0214ea69f554a031a0631e2c8
juanonsoftware <juanonsoftware>
2022-08-23 11:01:18 +0700

    Adding a class library

---
+++ Demo.Lib1/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo.Lib1
+{
+    public class Utils
+    {
+        public static string GetDate()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
--- Rabbit.Mvc5Minimal2/Controllers/HomeController.cs
+++ Rabbit.Mvc5Minimal2/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Demo.Lib1;
 using System.Web.Mvc;
 
 namespace Rabbit.Mvc5Minimal2.Controllers
@@ -10,6 +7,8 @@ namespace Rabbit.Mvc5Minimal2.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.DateTimeNow = Utils.GetDate();
+
             return View();
         }
 
